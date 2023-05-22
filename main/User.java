d22ec0f47142a08d3dbcce8cc14d01fef99e48c8
nadinehazem200 <131309409+nadinehazem200@users.noreply.github.com>
2023-05-22 21:34:53 +0300

    Add files via upload

---
+++ main/User.java
@@ -0,0 +1,27 @@
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User 
+{
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	protected int id;
+	protected String name;
+	//private String role;
+	
+}
