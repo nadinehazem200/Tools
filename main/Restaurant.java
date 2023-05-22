5321e22ce00610dcba5558b2042457133706bf2e
nadinehazem200 <131309409+nadinehazem200@users.noreply.github.com>
2023-05-22 23:23:47 +0300

    Add files via upload

---
+++ main/Restaurant.java
@@ -0,0 +1,67 @@
+import jakarta.ejb.LocalBean;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@LocalBean
+@Table(name="Restaurant")
+public class Restaurant
+{
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Set<Meal> getMeals() {
+		return meals;
+	}
+
+	public void setMeals(Set<Meal> meals) {
+		this.meals = meals;
+	}
+
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+
+	public RestaurantOwner getRestaurantOwner() {
+		return restaurantOwner;
+	}
+
+	public void setRestaurantOwner(RestaurantOwner restaurantOwner) {
+		this.restaurantOwner = restaurantOwner;
+	}
+
+	public Set<Order> getOrders() {
+		return orders;
+	}
+
+	public void setOrders(Set<Order> orders) {
+		this.orders = orders;
+	}
+
+	@ManyToOne
+	@JoinColumn(name = "ownerid")
+	private RestaurantOwner restaurantOwner;
+
+	@OneToMany(mappedBy = "restaurant")
+	private Set<Meal> meals;
+
+	@OneToMany(mappedBy = "restaurant")
+	private Set<Order> orders;
+	
+}
