2485193e32e8a70a7a329a60560b47a28b7f300a
nadinehazem200 <131309409+nadinehazem200@users.noreply.github.com>
2023-05-23 17:01:06 +0300

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,4 +1,7 @@
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import java.util.Set;
@@ -7,6 +10,22 @@ import java.util.Set;
 @Entity
 @Table(name="Customer")
 public class Customer extends User {
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	protected int id;
+	protected String name;
 
     @OneToMany(mappedBy = "customer")
     private Set<Order> orders;
