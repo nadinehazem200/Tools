1efa37d07a20379191157c37f81d556f3818ee39
nadinehazem200 <131309409+nadinehazem200@users.noreply.github.com>
2023-05-23 17:20:48 +0300

    Add files via upload

---
+++ OrderMealsDetails.java
@@ -0,0 +1,16 @@
+/*import javax.persistence.*;
+
+@Entity
+@Table(name = "OrderMealsDetails")
+public class OrderMealsDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @OneToOne
+    @JoinColumn(name = "orderId")
+    private int orderId;
+
+    @OneToMany
+    private int mealId;
+}*/
