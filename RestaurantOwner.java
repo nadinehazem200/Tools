c524ada03ee9ca467093a678873361df560f335e
nadinehazem200 <131309409+nadinehazem200@users.noreply.github.com>
2023-05-23 16:57:23 +0300

    Update RestaurantOwner.java

--- RestaurantOwner.java
+++ RestaurantOwner.java
@@ -3,7 +3,24 @@ import java.util.Set;
 
 @Entity
 @Table(name="RestaurantOwner")
-public class RestaurantOwner extends User{
+public class RestaurantOwner{
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	protected int id;
+	protected String name;
 	@OneToMany(mappedBy = "restaurantOwner")
 	private Set<Restaurant> restaurant;
 }
